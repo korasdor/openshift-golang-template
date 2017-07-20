67b3a437a29508d393df2ea1833f8271a9bcde39
koras_dor <koras_dor@yahoo.com>
2017-07-20 13:43:44 +0500

    test

--- example-golang-dep/cmd/server/main.go
+++ example-golang-dep/cmd/server/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"os"
+	"fmt"
 
 	"github.com/amsokol/openshift-golang-template/example-golang-dep/pkg/cmd/cli"
 	"github.com/amsokol/openshift-golang-template/example-golang-dep/pkg/cmd/server"
 )
 
 func main() {
+	fmt.Println("yes")
 	cli.EchoArgs()
 	if err := server.Start(cli.GetPort()); err != nil {
 		os.Exit(1)
