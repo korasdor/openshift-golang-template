dd0b93938b9a91ee313b5286ce495d4f61cd3362
koras_dor <koras_dor@yahoo.com>
2017-08-06 22:23:23 +0500

    add main.go

---
+++ colar-server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+
+	http.HandleFunc("/", hello)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "hello, world from %s", "s")
+}
